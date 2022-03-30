cb2e9c259b9ffdd6054b1aab75b50b7c0de78e43
Ruslan Guseynov <rslnguseynov@gmail.com>
2022-03-30 17:01:59 +0300

    add some tests

--- server.go
+++ server.go
@@ -4,13 +4,63 @@ import (
 	// "os"
 	"net/http"
 	// "html/template"
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+const dsn = "go:gopass@tcp(127.0.0.1)/urls"
+
+var (
+	db sql.DB
+	st sql.Stmt
+)
+
+func init() {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Can't create db")
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Can't establish a connection to database at dataSourceName %v", dsn)
+	}
+
+	st, err := db.Prepare("select * from urls where short = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = st
+}
+
 func main() {
+	// dsn := "go:gopass@tcp(127.0.0.1)/urls"
+	// db, err := sql.Open("mysql", dsn)
+	// if err != nil {
+	// 	log.Fatal("Can't create db")
+	// }
+	defer db.Close()
+
+	// if err := db.Ping(); err != nil {
+	// 	log.Fatalf("Can't establish a connection to database at dataSourceName %v", dsn)
+	// }
 	// http.HandleFunc("/", mainPage)
 	http.ListenAndServe(":8080", http.HandlerFunc(mainPage))
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	if params.Has("url") {
+		var full, short string
+		println("url =", params.Get("url"))
+		err := st.QueryRow(params.Get("url")).Scan(&full, &short)
+		if err != nil {
+			log.Fatal(err)
+		}
+		println("your short url:", short)
+	} else {
+		println("No url")
+	}
 	http.ServeFile(w, r, "index.html")
 }
--- sql.md
+++ sql.md
@@ -6,6 +6,6 @@ docker run --name mysql -e MYSQL_ROOT_PASSWORD=rootpass -e MYSQL_DATABASE=urls -
 
 create table urls ( Full varchar(2048) not null, Short varchar(255) not null unique, constraint pk_short primary key (Short));
 
-Insert into urls values (‘https://www.google.com/search?q=golang+log&ei=dlhAYur_Hcik3APNqZ_QCw&ved=0ahUKEwjqktiupeb2AhVIEncKHc3UB7oQ4dUDCA4&uact=5&oq=golang+log&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgcIABBHELADOgcIABCwAxBDOgQIABBDSgQIQRgASgQIRhgAUMgGWP4IYOAJaAJwAXgAgAFIiAHAAZIBATOYAQCgAQHIAQrAAQE&sclient=gws-wiz', ‘https://bit.ly/3JNb6D9’)
+Insert into urls values ('https://www.google.com/search?q=golang+log&ei=dlhAYur_Hcik3APNqZ_QCw&ved=0ahUKEwjqktiupeb2AhVIEncKHc3UB7oQ4dUDCA4&uact=5&oq=golang+log&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgcIABBHELADOgcIABCwAxBDOgQIABBDSgQIQRgASgQIRhgAUMgGWP4IYOAJaAJwAXgAgAFIiAHAAZIBATOYAQCgAQHIAQrAAQE&sclient=gws-wiz', 'https://bit.ly/3JNb6D9');
 
 414 (Request-URI Too Long)
