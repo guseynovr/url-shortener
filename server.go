6cef70daddb0346f1f0d60a2bbcfc5966f574f1e
guseynov <rslnguseynov@gmail.com>
2022-04-12 09:54:52 +0300

    create dbHandler

--- server.go
+++ server.go
@@ -2,60 +2,57 @@ package main
 
 import (
 	// "os"
+
 	"net/http"
+
 	// "html/template"
 	"database/sql"
 	"log"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 )
 
-const dsn = "go:gopass@tcp(127.0.0.1)/urls"
-
-var (
-	db sql.DB
-	st sql.Stmt
-)
+var h dbHandler
 
 func init() {
-	db, err := sql.Open("mysql", dsn)
+	cfg := mysql.Config{
+		User: "go",
+		Passwd: "gopass",
+		Net: "tcp",
+		Addr: "127.0.0.1:3306",
+		DBName: "urls",
+	}
+	db, err := sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal("Can't create db")
 	}
 
 	if err := db.Ping(); err != nil {
-		log.Fatalf("Can't establish a connection to database at dataSourceName %v", dsn)
+		log.Fatalf("Can't establish a connection to database at dataSourceName %v", cfg.FormatDSN())
 	}
 
-	st, err := db.Prepare("select * from urls where short = ?")
+	st, err := db.Prepare("select * from urls where full = ?")
 	if err != nil {
 		log.Fatal(err)
 	}
-	_ = st
+	h = dbHandler{db, st}
 }
 
-func main() {
-	// dsn := "go:gopass@tcp(127.0.0.1)/urls"
-	// db, err := sql.Open("mysql", dsn)
-	// if err != nil {
-	// 	log.Fatal("Can't create db")
-	// }
-	defer db.Close()
-
-	// if err := db.Ping(); err != nil {
-	// 	log.Fatalf("Can't establish a connection to database at dataSourceName %v", dsn)
-	// }
-	// http.HandleFunc("/", mainPage)
-	http.ListenAndServe(":8080", http.HandlerFunc(mainPage))
+type dbHandler struct {
+	db *sql.DB
+	st *sql.Stmt
 }
 
-func mainPage(w http.ResponseWriter, r *http.Request) {
+func (h dbHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	params := r.URL.Query()
 	if params.Has("url") {
 		var full, short string
 		println("url =", params.Get("url"))
-		err := st.QueryRow(params.Get("url")).Scan(&full, &short)
-		if err != nil {
+		err := h.st.QueryRow(params.Get("url")).Scan(&full, &short)
+		println("full", full, "\nshort", short)
+		if err == sql.ErrNoRows {
+			println("creating new short url")
+		} else if err != nil {
 			log.Fatal(err)
 		}
 		println("your short url:", short)
@@ -63,4 +60,11 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 		println("No url")
 	}
 	http.ServeFile(w, r, "index.html")
+}
+
+func main() {
+	defer h.db.Close()
+	defer h.st.Close()
+
+	http.ListenAndServe(":8080", h)
 }
